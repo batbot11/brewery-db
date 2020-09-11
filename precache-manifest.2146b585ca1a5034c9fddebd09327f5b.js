176d6603203f7da384422dc5d93c47383668b6f2
Tarun <pathaktarun1111@gmail.com>
2020-09-11 08:47:21 +0530

    Updates

---
+++ precache-manifest.2146b585ca1a5034c9fddebd09327f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068e38a98938abeb3ef8969f90f00d50",
+    "url": "/brewery-db/index.html"
+  },
+  {
+    "revision": "871525a7884c3a87bb0c",
+    "url": "/brewery-db/static/css/main.99f2cf8f.chunk.css"
+  },
+  {
+    "revision": "aacd5098a913ff62f065",
+    "url": "/brewery-db/static/js/2.6352d002.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brewery-db/static/js/2.6352d002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871525a7884c3a87bb0c",
+    "url": "/brewery-db/static/js/main.a5b198a6.chunk.js"
+  },
+  {
+    "revision": "57a7e619d55ee0c5527c",
+    "url": "/brewery-db/static/js/runtime-main.adaf2832.js"
+  }
+]);
